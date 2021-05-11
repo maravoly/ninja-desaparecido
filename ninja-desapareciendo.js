527c8e08b4edd4651b688e67b8725a356cd74694
maravoly <82736249+maravoly@users.noreply.github.com>
2021-05-11 15:33:33 -0700

    Add files via upload

---
+++ ninja-desapareciendo.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function(){
+    $(".zona").click(function(){
+        $(this).hide();
+    });
+});
+
+
+$(document).ready(function(){
+    $("#reset").click(function(){
+        $(".zona").show();
+    });
+});
+
